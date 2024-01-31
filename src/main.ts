126eef0e4c4ddcdd566cec22c28db407670e3d5c
aymen-ameri <92232283+aymen-ameri@users.noreply.github.com>
2024-01-31 14:50:38 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,12 @@ async function bootstrap() {
   const consulHost = 'consul'; // Replace with the Consul agent's host address
   const consul = new Consul({ host: consulHost });
 
-  const serviceName = 'nest'; // Replace with your service name
+  const serviceName = 'NEST-SERVICE'; // Replace with your service name
   const servicePort = 3002; // Replace with your service's port
+  const serviceId = 'NEST-SERVICE'; // Replace with a unique ID for your service
 
   const details = {
+    id: serviceId,
     name: serviceName,
     port: servicePort,
   };
@@ -21,7 +23,7 @@ async function bootstrap() {
     if (err) {
       console.error('Error registering with Consul:', err);
     } else {
-      console.log(`Registered with Consul as ${serviceName}`);
+      console.log(`Registered with Consul as ${serviceName} (ID: ${serviceId})`);
     }
   });
 
