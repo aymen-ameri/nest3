0572f65a8a196a34c4ce55b9c100fc54ac9ec1f3
aymen <ameri.aymen@esprit.tn>
2024-02-02 13:48:59 +0100

    6

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world abshore ';
+    return 'Hello world abshoree ';
   }
 }
